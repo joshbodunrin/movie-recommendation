cb7b589d41535671d524f572c9d850401a0e1581
joshbodunrin <83253910+joshbodunrin@users.noreply.github.com>
2024-08-05 21:29:41 -0400

    get dataset

--- get_movies.py
+++ get_movies.py
@@ -1,5 +1,31 @@
 import os
 import numpy as np
 import pandas as pd
-import matplotlib.pylot as plt
+#import matplotlib.pylot as plt
+
+
+ratings_df = pd.read_csv("../archive/ratings_small.csv")
+
+metadata_df = pd.read_csv("../archive/movies_metadata.csv", low_memory=False)
+
+#print(list(ratings_df.columns))
+
+#print(list(metadata_df.columns))
+
+metadata_df['id'] = pd.to_numeric(metadata_df['id'], errors='coerce')
+
+metadata_df = metadata_df.dropna(subset=['id'])
+
+metadata_df['id'] = metadata_df['id'].astype('int64')
+moviedata_df = pd.merge(ratings_df,metadata_df, left_on='movieId',right_on='id')
+
+moviedata_df = moviedata_df[['userId','movieId','rating','timestamp','genres','id','popularity','title','vote_average','vote_count']]
+
+moviedata_df = moviedata_df.dropna()
+
+
+print(list(moviedata_df.columns))
+
+print(moviedata_df)
+
 
